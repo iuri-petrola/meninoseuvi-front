c875119c8d0e69ef55b54597b5388bcfcf2b2ce5
Iuri Petrola <iuri.petrola@gmail.com>
2026-02-09 16:42:58 -0300

    Inclusao da seleçao do dia

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { MediaService } from '../../services/media.service';
+import { MediaItem } from '../../models/media-item';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +12,23 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+
+export class HomeComponent {
+  private readonly mediaService = inject(MediaService);
+
+  readonly selection$: Observable<MediaItem | null> = this.mediaService
+    .list()
+    .pipe(map((items) => this.pickRandomSelection(items)));
+
+  audioSrc(item: MediaItem): string {
+    return `data:${item.audioMimeType};base64,${item.audioBase64}`;
+  }
+
+  private pickRandomSelection(items: MediaItem[]): MediaItem | null {
+    if (!items || items.length === 0) return null;
+
+    const index = Math.floor(Math.random() * items.length);
+
+    return items[index];
+  }
+}
